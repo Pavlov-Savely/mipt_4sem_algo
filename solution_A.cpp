e91e83ff6b06249793c7f3e8adc1eea2d14c8a2b
Your Name <you@example.com>
2024-09-20 19:59:47 +0300

    Fixed

--- solution_A.cpp
+++ solution_A.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
-using namespace std; 
+class JoinView {
+public:
+  JoinView(const std::string &str, const std::string &obr, char delim = '#')
+      : str(str), obr(obr), delim(delim) {}
 
-vector<int> PrefixFunction(const string& s) {
-  int n = s.length();  
-  vector<int> pi(n);
-  for (int i = 1; i < n; ++i) {
+  char operator[](std::size_t index) const {
+    if (index < obr.length()) {
+      return obr[index];
+    } else if (index == obr.length()) {
+      return delim;
+    } else {
+      return str[index - obr.length() - 1];
+    }
+  }
+
+  std::size_t length() const { return str.length() + obr.length() + 1; }
+
+private:
+  const std::string &str;
+  const std::string &obr;
+  char delim;
+};
+
+void PrefixFunction(std::vector<int> &pi, const JoinView &s) {
+  const std::size_t &n = s.length();
+  pi[0] = 0;
+
+  for (std::size_t i = 1; i < n; ++i) {
     int j = pi[i - 1];
     while (j > 0 && s[i] != s[j]) {
       j = pi[j - 1];
@@ -17,34 +39,37 @@ vector<int> PrefixFunction(const string& s) {
     }
     pi[i] = j;
   }
-  return pi;
 }
 
-vector<int> FinOccurrences(const string& str, const string& obr) {
-  vector<int> positions;
+std::vector<int> FinOccurrences(const std::string &str, const std::string &obr) {
+  std::vector<int> positions;
   int obr_len = obr.length();
 
-  vector<int> p = PrefixFunction(obr + '#' + str);
+  JoinView joined(str, obr);
 
-  for (int i = obr_len + 1; i < p.size(); ++i) {
+  std::vector<int> p(joined.length());
+
+  PrefixFunction(p, joined);
+
+  for (std::size_t i = obr_len + 1; i < p.size(); ++i) {
     if (p[i] == obr_len) {
       positions.push_back(i - 2 * obr_len);
     }
   }
-  return positions;
+  return std::move(positions);
 }
 
 int main() {
-  string str;
-  string obr;
-  
-  cin >> str;
-  cin >> obr;
+  std::string str;
+  std::string obr;
 
-  vector<int> positions = FinOccurrences(str, obr);
+  std::cin >> str;
+  std::cin >> obr;
+
+  std::vector<int> &&positions = FinOccurrences(str, obr);
 
   for (int position : positions) {
-    cout << position << endl;
+    std::cout << position << std::endl;
   }
 
   return 0;
